76bbd05bb2c7e97ebc2dfcbc45804c39a1fedb6b
fairybugs <gilennimariel@gmail.com>
2026-07-30 12:27:30 -0400

    Co-authored-by-GilenniMoreno
    Se creó la estructura inicial del proyecto FraudShield con la solución y la aplicación de consola.

---
+++ src/FraudShield/FraudShield/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FraudShield;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("====================================");
+        Console.WriteLine("BIENVENIDO A FRAUDSHIELD");
+        Console.WriteLine("Sistema Inteligente de Detección de Fraude Bancario");
+        Console.WriteLine("====================================");
+    }
+}
