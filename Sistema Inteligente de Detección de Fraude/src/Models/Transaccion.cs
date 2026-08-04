4239c1eddd40e9a68e744c8b1a3293ffadd9ff05
Oswaldo-Cuevas <oswaldocuevasfeliz2014@gmail.com>
2026-08-03 17:22:57 -0700

    Reemplazo Transaccion.cs en carpeta correcta Models
    Co-authored-by: OswaldoCuevas

---
+++ Sistema Inteligente de Detección de Fraude/src/Models/Transaccion.cs
@@ -0,0 +1,25 @@
+﻿using CsvHelper.Configuration.Attributes;
+namespace FraudShield.Models;
+
+public class Transaccion
+{
+    public long Id { get; set; }
+
+    public string ClienteId { get; set; } = string.Empty;
+
+    public decimal Monto { get; set; }
+
+    public string Pais { get; set; } = string.Empty;
+
+    public string PaisHabitual { get; set; } = string.Empty;
+
+    public DateTime FechaHora { get; set; }
+
+    public string Comercio { get; set; } = string.Empty;
+
+    public string Categoria { get; set; } = string.Empty;
+
+    public string MetodoPago { get; set; } = string.Empty;
+
+    public bool EsFraudeReal { get; set; }
+}
