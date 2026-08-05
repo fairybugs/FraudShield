3928fc1b71c9913765fb96a367e60abd7ddf0c41
fairybugs <gilennimariel@gmail.com>
2026-08-05 17:25:52 -0400

    Co-authored-by-GilenniMoreno
    Creación de pruebas para ver si funcionan correcto los analizadores

---
+++ src/FraudShield/Tests/AnalizadorComercioTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FraudShield.Analizadores;
+using FraudShield.Models;
+using Xunit;
+
+namespace Tests;
+
+public class AnalizadorComercioTests
+{
+    [Fact]
+    public void DebeDetectarComercioRiesgoso()
+    {
+        var analizador = new AnalizadorComercio();
+
+        var transaccion = new Transaccion
+        {
+            Comercio = "Crypto Exchange"
+        };
+
+        var resultado = analizador.Analizar(transaccion);
+
+        Assert.True(resultado.EsSospechosa);
+        Assert.Equal(15, resultado.Riesgo);
+    }
+
+    [Fact]
+    public void NoDebeDetectarComercioNormal()
+    {
+        var analizador = new AnalizadorComercio();
+
+        var transaccion = new Transaccion
+        {
+            Comercio = "Jumbo"
+        };
+
+        var resultado = analizador.Analizar(transaccion);
+
+        Assert.False(resultado.EsSospechosa);
+        Assert.Equal(0, resultado.Riesgo);
+    }
+}
