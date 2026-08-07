0d63c9411d02314afa5b25c80507eb00fd47f3f0
Gabrielpaulino2023-1083 <gabriel.px90@gmail.com>
2026-08-07 13:22:38 -0400

    Co-authored-by-GabrielPaulino
    Creación de la clase resultado medicion

---
+++ src/FraudShield/FraudShield/Metrics/ResultadoMedicion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FraudShield.Models;
+
+namespace FraudShield.Metrics;
+
+public class ResultadoMedicion
+{
+    public long Tiempo { get; set; }
+    public List<ResultadoAnalisis> Resultados { get; set; } = new();
+}
+
